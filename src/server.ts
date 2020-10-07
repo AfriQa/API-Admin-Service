84e8e1e092d92fa13204d6f1c8a70f4787dbe4ce
async69 <yeabsera0830@gmail.com>
2020-10-08 00:53:09 +0300

    Added Port

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ server.applyMiddleware({ app, path: '/graphql' })
 
 const httpServer = createServer(app)
 httpServer.listen(
-    { port: 4000 },
+    { port: process.env.PORT || 4000 },
     (): void => {
         console.log(`\n🚀 GraphQL is now running on http://localhost:4000/graphql`)
     }
