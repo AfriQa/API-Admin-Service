db35a8edd5cc7e3291ee9affc5b07de986d35839
async69 <yeabsera0830@gmail.com>
2020-10-08 01:26:02 +0300

    Small fixes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import { ApolloServer } from "apollo-server-express"
-// import depthLimit from "graphql-depth-limit"
+import depthLimit from "graphql-depth-limit"
 import { createServer } from "http"
 import compression from "compression"
 import cors from "cors"
@@ -9,7 +9,7 @@ import schema from "./schema"
 const app = express()
 const server = new ApolloServer({
     schema,
-    // validationRules: [depthLimit(7)],
+    validationRules: [depthLimit(7)],
     introspection: true
 })
 
